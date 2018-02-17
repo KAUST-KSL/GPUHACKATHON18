56965cc95d4853cd4fda0ae8c58146fd8a57c183
George Markomanolis <george@markomanolis.com>
2018-02-17 20:22:18 +0300

    Create submit_profiling_file.sh
    submission file to profile and save the data in nvprof file for visualization with nvvp

---
+++ submit_profiling_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#SBATCH --partition=batch 
+#SBATCH --job-name="test" 
+#SBATCH --gres=gpu:p100:1
+#SBATCH --res=HACKATHON
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1
+#SBATCH --time=00:10:00 
+#SBATCH --exclusive 
+#SBATCH --err=JOB.%j.err 
+#SBATCH --output=JOB.%j.out 
+#--------------------------------------------# 
+module load cuda/9.0.176
+module load pgi/17.10
+srun -n 1 nvprof -o results.nvprof --cpu-profiling on ./laplace_serial
